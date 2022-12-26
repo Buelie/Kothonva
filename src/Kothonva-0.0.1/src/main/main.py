2c24d42ab47f61281e5f8efb6edf0605cfa16e17
LYErikuy <111875719+Buelie@users.noreply.github.com>
2022-12-26 20:41:17 +0800

    Update main.py

--- src/Kothonva-0.0.1/src/main/main.py
+++ src/Kothonva-0.0.1/src/main/main.py
@@ -1,35 +1,20 @@
 import random
-import logging
 
-class syst:
-    def __init__(self , ner):
-        self.ner = ner
-    def print(self):
-        print(str(self.ner))
-        return 0
-class add:
+class cal:
     def __init__(self , one , two):
-        self.one = one
-        self.two = two
-    def cont(self):
+        self.one = float(one)
+        self.two = float(two)
+    def add(self):
         val = self.one + self.two
-        print("log : cont(add) is"+val)
         return val
-class sub:
-    def __init__(self , one , two):
-        self.one = one
-        self.two = two
-    def cont(self):
+    def sub(self):
         val = self.one - self.two
-        print("log : cont(sub) is"+val)
         return val
-class mul:
-    def __init__(self , one , two):
-        self.one = one
-        self.two = two
-    def cont(self):
+    def mul(self):
         val = self.one * self.two
-        print("log : cont(mul) is"+val)
+        return val
+    def div(self):
+        val = self.one / self.two
         return val
 class random:
     def __init__(self , one , two):
@@ -37,19 +22,7 @@ class random:
         self.two = two
     def choice(self):
         val = random.choice(self.one)
-        print("log : random choice >"+val)
         return val
     def randint(self):
-        val = random.randint(self.one , self.two)
-        print("log : random randint >"+val)
+        val = random.randint(int(self.one),int(self.two))
         return val
-class classes:
-    def __init__(self , one , two):
-        self.one = one
-        self.two = two
-    def add(self):
-        name = self.one
-        print("creat a class >"+name)
-    def dele(self):
-        name = self.one
-        del name
