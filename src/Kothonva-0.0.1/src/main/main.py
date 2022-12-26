5aafd4f558d7e291728fca859dd90f7f2546de98
LYErikuy <111875719+Buelie@users.noreply.github.com>
2022-12-26 18:34:00 +0800

    Create main.py

---
+++ src/Kothonva-0.0.1/src/main/main.py
@@ -0,0 +1,44 @@
+import random
+
+class syst:
+    def __init__(self , ner):
+        self.ner = ner
+    def print(self):
+        print(str(self.ner))
+        return 0
+class add:
+    def __init__(self , one , two):
+        self.one = one
+        self.two = two
+    def cont(self):
+        val = self.one + self.two
+        print("log : cont(add) is"+val)
+        return val
+class sub:
+    def __init__(self , one , two):
+        self.one = one
+        self.two = two
+    def cont(self):
+        val = self.one - self.two
+        print("log : cont(sub) is"+val)
+        return val
+class mul:
+    def __init__(self , one , two):
+        self.one = one
+        self.two = two
+    def cont(self):
+        val = self.one * self.two
+        print("log : cont(mul) is"+val)
+        return val
+class random:
+    def __init__(self , one , two):
+        self.one = one
+        self.two = two
+    def choice(self):
+        val = random.choice(self.one)
+        print("log : random choice >"+val)
+        return val
+    def randint(self):
+        val = random.randint(self.one , self.two)
+        print("log : random randint >"+val)
+        return val
