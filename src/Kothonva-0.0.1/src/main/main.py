eb0c309437a5581b5bf8e647555e5deef3ff34b5
LYErikuy <111875719+Buelie@users.noreply.github.com>
2022-12-27 09:09:05 +0800

    Update main.py

--- src/Kothonva-0.0.1/src/main/main.py
+++ src/Kothonva-0.0.1/src/main/main.py
@@ -2,27 +2,54 @@ import random
 
 class cal:
     def __init__(self , one , two):
-        self.one = float(one)
-        self.two = float(two)
+        self.one = one
+        self.two = two
     def add(self):
-        val = self.one + self.two
-        return val
+        try:
+            val = self.one + self.two
+            return val
+        except (TypeError,KeyError,ValueError,AttributeError,AssertionError,ArithmeticError):
+            print("Error:Cannot be calculated")
     def sub(self):
-        val = self.one - self.two
-        return val
+        try:
+            val = self.one - self.two
+            return val
+        except (TypeError,KeyError,ValueError,AttributeError,AssertionError,ArithmeticError):
+            print("Error:Cannot be calculated")
     def mul(self):
-        val = self.one * self.two
-        return val
+        try:
+            val = self.one * self.two
+            return val
+        except (TypeError,KeyError,ValueError,AttributeError,AssertionError,ArithmeticError):
+            print("Error:Cannot be calculated")
     def div(self):
-        val = self.one / self.two
-        return val
+        try:
+            val = self.one / self.two
+            return val
+        except (TypeError,KeyError,ValueError,AttributeError,AssertionError,ArithmeticError):
+            print("Error:Cannot be calculated")
 class random:
     def __init__(self , one , two):
         self.one = one
         self.two = two
     def choice(self):
-        val = random.choice(self.one)
-        return val
+        try:
+            val = random.choice(self.one)
+            return val
+        except  (TypeError,KeyError,ValueError,AttributeError,AssertionError,ArithmeticError):
+            print("Error:Random generation failed")
     def randint(self):
-        val = random.randint(int(self.one),int(self.two))
-        return val
+        try:
+            val = random.randint(int(self.one),int(self.two))
+            return val
+        except  (TypeError,KeyError,ValueError,AttributeError,AssertionError,ArithmeticError):
+            print("Error:Random generation failed")
+class type:
+    def __init__(self , one):
+        self.one = one
+    def val(self):
+        try:
+            val = type(self.one)
+            return val
+        except  (TypeError,KeyError,ValueError,AttributeError,AssertionError,ArithmeticError):
+            print("Error:The type could not be detected")
